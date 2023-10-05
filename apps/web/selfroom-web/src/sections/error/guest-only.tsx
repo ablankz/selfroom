90c3a0ae9489a9bfec7f579108c967028f8917b7
blank <hayaken3007@i.softbank.jp>
2023-10-05 22:39:34 +0900

    ゲストオンリーのガードページ作成

---
+++ apps/web/selfroom-web/src/sections/error/guest-only.tsx
@@ -0,0 +1,43 @@
+import { m } from 'framer-motion';
+// @mui
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// assets
+import { ForbiddenIllustration } from '@/assets/illustrations';
+// components
+import { RouterLink } from '@/routes/components';
+import { MotionContainer, varBounce } from '@/components/animate';
+import { useLocales } from '@/locales';
+import { PATH_AFTER_LOGIN } from '@/config-global';
+
+// ----------------------------------------------------------------------
+
+export default function GuestOnly() {
+  const { t } = useLocales();
+
+  return (
+    <MotionContainer>
+      <m.div variants={varBounce().in}>
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          Guest Only Page
+        </Typography>
+      </m.div>
+
+      <m.div variants={varBounce().in}>
+        <Typography sx={{ color: 'text.secondary' }}>
+          {t('Authenticated users cannot view this page')}
+          <br />
+          {t('You can browse main content')}
+        </Typography>
+      </m.div>
+
+      <m.div variants={varBounce().in}>
+        <ForbiddenIllustration sx={{ height: 260, my: { xs: 5, sm: 10 } }} />
+      </m.div>
+
+      <Button component={RouterLink} href={PATH_AFTER_LOGIN} size="large" variant="contained">
+        {t('Go to Main Content')}
+      </Button>
+    </MotionContainer>
+  );
+}
