b4700992370a4a51302ce19bdbdbb1ecb783d05f
blank <hayaken3007@i.softbank.jp>
2023-10-05 19:13:18 +0900

    provider定義ファイル作成

---
+++ apps/web/selfroom-web/src/sections/dashboard/auth-login/auth-providers.ts
@@ -0,0 +1,40 @@
+import { SocialProvider } from '@/types/social-provider';
+import { ButtonPropsColorOverrides } from '@mui/material';
+import { OverridableStringUnion } from '@mui/types';
+
+type Propvider = {
+  value: SocialProvider;
+  icon: string;
+  iconColor?: string;
+  iconWidth?: number;
+  label: string;
+  jaLabel: string;
+  color?: OverridableStringUnion<
+    | 'inherit'
+    | 'success'
+    | 'primary'
+    | 'secondary'
+    | 'error'
+    | 'info'
+    | 'warning',
+    ButtonPropsColorOverrides
+  >;
+};
+
+export const AUTH_PROVIDERS: Propvider[] = [
+  {
+    value: 'line',
+    icon: 'bi:line',
+    iconColor: '#06C755',
+    iconWidth: 22,
+    color: 'success',
+    label: 'Log in with LINE',
+    jaLabel: 'LINEでログイン',
+  },
+  {
+    value: 'google',
+    icon: 'devicon:google',
+    label: 'Log in with Google',
+    jaLabel: 'Googleでログイン',
+  },
+];
