7881ccbcbd66b422c2ce89ece6556ef089cd22e3
blank <hayaken3007@i.softbank.jp>
2023-10-05 22:41:46 +0900

    ゲストガードの引っかかったときの処理変更

--- apps/web/selfroom-web/src/auth/guard/guest-guard.tsx
+++ apps/web/selfroom-web/src/auth/guard/guest-guard.tsx
@@ -1,9 +1,11 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 // routes
 import { useRouter, useSearchParams } from '@/routes/hooks';
 //
 import { useAuthContext } from '../hooks';
 import { PATH_AFTER_LOGIN } from '@/config-global';
+import MinimamLayout from '@/layouts/minimam-layout';
+import GuestOnly from '@/sections/error/guest-only';
 
 // ----------------------------------------------------------------------
 
@@ -15,20 +17,29 @@ export default function GuestGuard({ children }: Props) {
   const router = useRouter();
 
   const searchParams = useSearchParams();
+  const [auth, setAuth] = useState(false);
 
   const returnTo = searchParams.get('returnTo') || PATH_AFTER_LOGIN;
 
   const { authenticated } = useAuthContext();
 
   const check = useCallback(() => {
-    if (authenticated) {
-      router.replace(returnTo);
-    }
+    // if (authenticated) {
+    //   router.replace(returnTo);
+    // }
+    setAuth(authenticated);
   }, [authenticated, returnTo, router]);
 
   useEffect(() => {
     check();
   }, [check]);
+
+  if (auth)
+    return (
+      <MinimamLayout>
+        <GuestOnly />
+      </MinimamLayout>
+    );
 
   return <>{children}</>;
 }
