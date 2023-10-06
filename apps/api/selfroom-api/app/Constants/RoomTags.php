9726007b67b517d0bc628b736b3c93b2feb7e76f
blank <hayaken3007@i.softbank.jp>
2023-10-07 01:24:57 +0900

    初期のルームカテゴリー定義記述

--- apps/api/selfroom-api/app/Constants/RoomTags.php
+++ apps/api/selfroom-api/app/Constants/RoomTags.php
@@ -6,4 +6,36 @@ class RoomTags
 {
   public const ROOM_MIN_TAGS = 1;
   public const ROOM_MAX_TAGS = 3;
+
+  public const INITIAL_ROOM_CATEGORIES = [
+    "Anyone can join",
+    "Private",
+    "IT",
+    "Small talk",
+    "Game",
+    "Sport",
+    "Celebrities & Entertainers",
+    "Same generation",
+    "Animation & Manga",
+    "Finance & Business",
+    "Music",
+    "Community & Living",
+    "Fashion & Beauty",
+    "Illustration",
+    "Research & Study",
+    "Working & Job",
+    "Schools & Alumni Associations",
+    "Cooking & Gourmet",
+    "Health",
+    "Organization",
+    "Fertility & Childcare",
+    "Vehicle",
+    "Photograph",
+    "Traveling",
+    "Animals & Pets",
+    "TV & VOD",
+    "Reading book",
+    "Film & Stage",
+    "Other"
+  ];
 }
