a93c1062b371498ca668370c14e889e4e6585f3d
blank <hayaken3007@i.softbank.jp>
2023-10-07 03:36:25 +0900

    認証情報の取得にユースケース適用

--- apps/api/selfroom-api/app/Http/Controllers/Account/RetrieveAction.php
+++ apps/api/selfroom-api/app/Http/Controllers/Account/RetrieveAction.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use App\Usecases\User\FindUser;
 use Illuminate\Auth\AuthManager;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -10,14 +11,22 @@ use Illuminate\Http\Request;
 class RetrieveAction extends Controller
 {
   private $authManager;
+  private $userFind;
+  private $adminFind;
 
-  public function __construct(AuthManager $authManager)
+  public function __construct(AuthManager $authManager, FindUser $userFind)
   {
     $this->authManager = $authManager;
+    $this->userFind = $userFind;
   }
 
   public function __invoke(Request $request): JsonResponse
   {
-    return response()->success($this->authManager->guard('jwt')->user());
+    $user = $this->authManager->guard('jwt')->user();
+    if($user->admin_id){
+      return response()->success($this->adminFind->handle($user->admin_id));
+    }else{
+      return response()->success($this->userFind->handle($user->user_id));
+    }
   }
 }
