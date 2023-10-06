49fd3f1ca1bf6bd08e09d2939fe044d12b9231a2
blank <hayaken3007@i.softbank.jp>
2023-10-07 01:25:35 +0900

    マスタテーブルの初期データシーダー生成

---
+++ apps/api/selfroom-api/database/seeders/InitialDataSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class InitialDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      $this->call(InitialRoleSeeder::class);
+      $this->call(InitialRoomCategorySeeder::class);
+    }
+}
