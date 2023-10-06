6c0bc9752cd7bc1d3747797a8ec9596d43a3483d
blank <hayaken3007@i.softbank.jp>
2023-10-06 22:47:49 +0900

    不要ファクトリの削除

--- apps/api/selfroom-api/database/factories/AccountFactory.php
+++ apps/api/selfroom-api/database/factories/AccountFactory.php
@@ -19,7 +19,6 @@ class AccountFactory extends Factory
         return [
             'login_id' => $this->faker->unique()->safeEmail,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'is_active' => $this->faker->boolean,
         ];
     }
 }
