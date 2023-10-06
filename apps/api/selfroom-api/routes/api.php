83a2e583bbc06a268b44080b641fa9ec9608bdf7
blank <hayaken3007@i.softbank.jp>
2023-10-07 03:32:12 +0900

    ルートの整理

--- apps/api/selfroom-api/routes/api.php
+++ apps/api/selfroom-api/routes/api.php
@@ -14,41 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('auth')->group(function () {
-  Route::post('login', \App\Http\Controllers\Account\LoginAction::class);
+  Route::post('login', \App\Http\Controllers\Account\LoginAction::class)->middleware('guest:jwt');
   Route::post('user', \App\Http\Controllers\Account\RetrieveAction::class)->middleware('auth:jwt');
   Route::post('logout', \App\Http\Controllers\Account\LogoutAction::class)->middleware('auth:jwt');
   Route::post('refresh', \App\Http\Controllers\Account\RefreshTokenAction::class);
   // ソーシャルログイン
   Route::get('{provider}', \App\Http\Controllers\Account\OAuthRedirectAction::class)
-    ->where('provider', '(google)|(line)');
+    ->where('provider', '(google)|(line)')->middleware('guest:jwt');
   Route::get('/{provider}/callback', App\Http\Controllers\Account\OAuthHandleCallbackAction::class)
     ->where('provider', '(google)|(line)');
 });
 
-Route::prefix('clients')->group(function () {
-  Route::get('{id}', [\App\Http\Controllers\ClientController::class, 'find']);
-  Route::get('', [\App\Http\Controllers\ClientController::class, 'findAll']);
-  Route::post('', [\App\Http\Controllers\ClientController::class, 'create']);
-  Route::put('{id}', [\App\Http\Controllers\ClientController::class, 'update']);
-  Route::delete('{id}', [\App\Http\Controllers\ClientController::class, 'delete']);
-});
-
-Route::prefix('shops')->group(function () {
-  Route::get('{id}', [\App\Http\Controllers\ShopController::class, 'find']);
-  Route::get('', [\App\Http\Controllers\ShopController::class, 'findAll']);
-  Route::post('', [\App\Http\Controllers\ShopController::class, 'create']);
-  Route::put('{id}', [\App\Http\Controllers\ShopController::class, 'update']);
-  Route::delete('{id}', [\App\Http\Controllers\ShopController::class, 'delete']);
-});
-
-Route::prefix('partners')->group(function () {
-  Route::get('{id}', [\App\Http\Controllers\PartnerController::class, 'find']);
-  Route::get('', [\App\Http\Controllers\PartnerController::class, 'findAll']);
-  Route::post('', [\App\Http\Controllers\PartnerController::class, 'create']);
-  Route::put('{id}', [\App\Http\Controllers\PartnerController::class, 'update']);
-  Route::delete('{id}', [\App\Http\Controllers\PartnerController::class, 'delete']);
-});
-
-Route::get('error', function () {
-  throw new \App\Exceptions\ApplicationException(\App\Enums\ApplicationCode::TokenMismatch);
-});
+// Route::prefix('clients')->group(function () {
+//   Route::get('{id}', [\App\Http\Controllers\ClientController::class, 'find']);
+//   Route::get('', [\App\Http\Controllers\ClientController::class, 'findAll']);
+//   Route::post('', [\App\Http\Controllers\ClientController::class, 'create']);
+//   Route::put('{id}', [\App\Http\Controllers\ClientController::class, 'update']);
+//   Route::delete('{id}', [\App\Http\Controllers\ClientController::class, 'delete']);
+// });
