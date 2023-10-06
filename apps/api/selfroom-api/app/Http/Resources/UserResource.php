7634402d717f9bb874094aced4ece90916988438
blank <hayaken3007@i.softbank.jp>
2023-10-07 03:34:45 +0900

    userApiリソース生成

---
+++ apps/api/selfroom-api/app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+  /**
+   * Transform the resource into an array.
+   *
+   * @return array<string, mixed>
+   */
+  public function toArray(Request $request): array
+  {
+    return [
+      'userId' => $this->user_id,
+      'nickname' => $this->nickname,
+      'profilePhotoUrl' => $this->profile_photo_url,
+      'followerNum' => $this->follower_num,
+      'followNum' => $this->follow_num,
+      'favoriteRoomNum' => $this->favorite_room_num,
+      'currentChatRoom' => $this->current_chat_room,
+      'createdAt' => $this->created_at,
+      'updatedAt' => $this->updated_at,
+    ];
+  }
+}
