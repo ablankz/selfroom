c0d61b16ef86afa7b666347b6cc89f80260f9e62
blank <hayaken3007@i.softbank.jp>
2023-10-08 02:56:45 +0900

    ログインリクエストを作成

---
+++ apps/api/selfroom-api/app/Http/Requests/Account/LoginRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Account;
+
+use App\Http\Requests\ApiRequest;
+
+class LoginRequest extends ApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'loginId' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ];
+    }
+}
