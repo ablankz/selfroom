ab252b0f21f1acea63088ebf269116e8957fef61
blank <hayaken3007@i.softbank.jp>
2023-10-08 02:53:46 +0900

    throtteの設定を定数化

---
+++ apps/api/selfroom-api/app/Constants/ThrottleSettings.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class ThrottleSettings
+{
+  public const LOGIN_MAX_ATTEMPTS = 5;
+  public const LOGIN_DECAY_SECONDS = 180;
+
+  public const WHOLE_MAX_ATTEMPTS = 30;
+  public const WHOLE_DECAY_MINUTES = 1;
+}
