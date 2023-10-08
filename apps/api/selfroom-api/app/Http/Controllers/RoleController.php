8ce175b776e22d4b18dd208d933a04448de39298
blank <hayaken3007@i.softbank.jp>
2023-10-08 22:56:31 +0900

    roleコントローラの作成

---
+++ apps/api/selfroom-api/app/Http/Controllers/RoleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Role\ViewRoleRequest;
+use App\Services\RoleService;
+use Illuminate\Http\JsonResponse;
+
+class RoleController extends Controller
+{
+  protected $service;
+
+  public function __construct(RoleService $service)
+  {
+    $this->service = $service;
+  }
+
+  public function find(ViewRoleRequest $request, int $id): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'find'],
+      ['role_id' => $id]
+    ));
+  }
+
+  public function get(ViewRoleRequest $request): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'get']
+    ));
+  }
+}
