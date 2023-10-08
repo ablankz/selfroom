f445edaad19e23b0f0e44ebccb21be7f23e98aaf
blank <hayaken3007@i.softbank.jp>
2023-10-08 12:31:07 +0900

    ストレージに関する定数化

---
+++ apps/api/selfroom-api/app/Constants/StorageSettings.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Constants;
+
+class StorageSettings
+{
+  public const USER_PROFILE_STORAGE = 'public/user-profile-photo';
+  public const ADMIN_PROFILE_STORAGE = 'public/admin-profile-photo';
+}
