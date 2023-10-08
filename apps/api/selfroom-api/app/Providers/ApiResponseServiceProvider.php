37ae51f11a4216680b13e7209d1a67d15ffc2c4f
blank <hayaken3007@i.softbank.jp>
2023-10-08 14:11:05 +0900

    apiレスポンスメッセージを多言語対応するための準備

--- apps/api/selfroom-api/app/Providers/ApiResponseServiceProvider.php
+++ apps/api/selfroom-api/app/Providers/ApiResponseServiceProvider.php
@@ -34,7 +34,7 @@ class ApiResponseServiceProvider extends ServiceProvider
         'success'  => true,
         'data' => $data,
         'code' => ApplicationCode::Success,
-        'message' => "OK",
+        'message' => __("response.". ApplicationCode::Success->getText()),
         'errorAttributes' => [], // エラー用
       ], $statusCode);
     });
@@ -45,7 +45,7 @@ class ApiResponseServiceProvider extends ServiceProvider
         'success'  => false,
         'data' => null,
         'code' => $errorCode->value,
-        'message' => $errorCode->getText(),
+        'message' => __("response.{$errorCode->getText()}"),
         'errorAttributes' => $errors, // エラー用
         '_debugMessage' => $e->getMessage(),
         '_exception' => get_class($e),
