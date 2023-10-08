4a3fac4877002afd5219ef36ccb9ea2b640f5427
blank <hayaken3007@i.softbank.jp>
2023-10-09 00:30:36 +0900

    チャットルームコントローラ作成

---
+++ apps/api/selfroom-api/app/Http/Controllers/ChatRoomController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ChatRoom\StoreChatRoomRequest;
+use App\Services\ChatRoomService;
+use Illuminate\Http\JsonResponse;
+
+class ChatRoomController extends Controller
+{
+  protected $service;
+
+  public function __construct(ChatRoomService $service)
+  {
+    $this->service = $service;
+  }
+
+  public function find(string $uuid): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'find'],
+      ['chat_room_id' => $uuid]
+    ));
+  }
+
+  public function get(): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'get']
+    ));
+  }
+
+  public function create(StoreChatRoomRequest $request): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'create'],
+      [
+        'name' => $request->get('name'),
+        'cover_photo_url' => $request->file('coverPhoto'),
+        'room_key' => $request->get('roomKey')
+      ]
+    ));
+  }
+}
