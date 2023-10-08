aede07c1a0a041a0910391a69264023fc02d8f4e
blank <hayaken3007@i.softbank.jp>
2023-10-08 22:37:16 +0900

    間違え訂正

--- apps/api/selfroom-api/app/Models/RoomCategory.php
+++ apps/api/selfroom-api/app/Models/RoomCategory.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RoomCategory extends Model
 {
-  use HasFactory, SoftDeletes;
+  use HasFactory;
 
   protected $table = 'm_room_categories';
   protected $primaryKey = 'room_category_id';
