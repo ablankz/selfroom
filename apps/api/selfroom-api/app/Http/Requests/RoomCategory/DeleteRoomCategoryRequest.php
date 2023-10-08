417e03b9d68fd5ccc95db07245f32833798cf463
blank <hayaken3007@i.softbank.jp>
2023-10-08 22:38:06 +0900

    roomCategory関係のフォームリクエスト作成

---
+++ apps/api/selfroom-api/app/Http/Requests/RoomCategory/DeleteRoomCategoryRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\RoomCategory;
+
+use App\Enums\Role\CategoryRole;
+use App\Http\Requests\ApiRequest;
+use Illuminate\Support\Facades\Gate;
+
+class DeleteRoomCategoryRequest extends ApiRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return Gate::check(CategoryRole::Delete->value);
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+   */
+  public function rules(): array
+  {
+    return [];
+  }
+}
---
+++ apps/api/selfroom-api/app/Http/Requests/RoomCategory/UpdateRoomCategoryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\RoomCategory;
+
+use App\Enums\Role\CategoryRole;
+use App\Http\Requests\ApiRequest;
+use Illuminate\Support\Facades\Gate;
+
+class UpdateRoomCategoryRequest extends ApiRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return Gate::check(CategoryRole::Edit->value);
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'name' => ['required', 'string'],
+    ];
+  }
+}
