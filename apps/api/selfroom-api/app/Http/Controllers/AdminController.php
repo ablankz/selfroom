938ef0236656f612bd4a3f31c16881e760ca80bd
blank <hayaken3007@i.softbank.jp>
2023-10-08 16:38:37 +0900

    adminコントローラ作成

---
+++ apps/api/selfroom-api/app/Http/Controllers/AdminController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Admin\StoreAdminRequest;
+use App\Http\Requests\Admin\UpdateAdminRequest;
+use App\Services\AdminService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+  protected $service;
+
+  public function __construct(AdminService $service)
+  {
+    $this->service = $service;
+  }
+
+  public function find(string $uuid): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'find'],
+      ['admin_id' => $uuid]
+    ));
+  }
+
+  public function get(): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'get']
+    ));
+  }
+
+  public function create(StoreAdminRequest $request): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'create'],
+      [
+        'created_by' => $request->user()->admin_id,
+        'login_id' => $request->get('loginId'),
+        'raw_password' => $request->get('password'),
+        'nickname' => $request->get('nickname'),
+        'profile_photo_url' => $request->file('profilePhotoUrl'),
+
+      ]
+    ));
+  }
+
+  public function update(UpdateAdminRequest $request): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'update'],
+      [
+        'admin_id' => $request->admin()->admin_id,
+        'nickname' => $request->get('nickname'),
+        'profile_photo_url' => $request->file('profilePhotoUrl'),
+      ]
+    ));
+  }
+
+  public function delete(Request $request): JsonResponse
+  {
+    return response()->success(app()->call(
+      [$this->service, 'delete'],
+      ['admin_id' => $request->admin()->admin_id]
+    ));
+  }
+}
