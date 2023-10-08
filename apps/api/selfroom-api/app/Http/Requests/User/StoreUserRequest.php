f9772e2e0b816d15f329356864cc74304ca4c7d0
blank <hayaken3007@i.softbank.jp>
2023-10-08 12:33:01 +0900

    ルールの変更

--- apps/api/selfroom-api/app/Http/Requests/User/StoreUserRequest.php
+++ apps/api/selfroom-api/app/Http/Requests/User/StoreUserRequest.php
@@ -3,42 +3,43 @@
 namespace App\Http\Requests\User;
 
 use App\Http\Requests\ApiRequest;
+use Illuminate\Validation\Rules\Password;
 
 class StoreUserRequest extends ApiRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * @var array キャストルール
-     */
-    protected $casts = [
-        'is_open' => 'bool',
+  /**
+   * Get data to be validated from the request.
+   *
+   * @return array
+   */
+  public function validationData()
+  {
+    return [
+      ...$this->only(['name', 'profilePhotoUrl', 'loginId', 'password']),
+      'password_confirmation' => $this->get('confirmPassword')
     ];
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'name' => ['required', 'string', 'max:15'],
-            'address' => ['required', 'string'],
-            'mailaddress' => ['required', 'string', 'email'],
-            'site_url' => ['required', 'string', 'url'],
-            'tel' => ['required', 'string'],
-            'fax' => ['required', 'string'],
-            'charge' => ['required', 'string'],
-            'plan' => ['required', 'string'],
-            'design' => ['required', 'string'],
-            'is_open' => ['required', 'boolean']
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'name' => ['required', 'string'],
+      'profilePhotoUrl' => ['file', 'max:10240', 'mimes:jpg,jpeg,png,gif'],
+      'loginId' => ['required', 'string', 'unique:App\Models\Account,login_id'],
+      'password' => ['required', 'string', new Password(6), 'confirmed'],
+    ];
+  }
 }
--- apps/api/selfroom-api/app/Http/Requests/User/UpdateUserRequest.php
+++ apps/api/selfroom-api/app/Http/Requests/User/UpdateUserRequest.php
@@ -6,32 +6,24 @@ use App\Http\Requests\ApiRequest;
 
 class UpdateUserRequest extends ApiRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'name' => ['required', 'string', 'max:15'],
-            'address' => ['required', 'string'],
-            'mailaddress' => ['required', 'string', 'email'],
-            'site_url' => ['required', 'string', 'url'],
-            'tel' => ['required', 'string'],
-            'fax' => ['required', 'string'],
-            'charge' => ['required', 'string'],
-            'plan' => ['required', 'string'],
-            'design' => ['required', 'string'],
-            'is_open' => ['required', 'boolean']
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'name' => ['required', 'string'],
+      'profilePhotoUrl' => ['file', 'max:10240', 'mimes:jpg,jpeg,png,gif'],
+    ];
+  }
 }
