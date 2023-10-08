1d5ddb44b14b15cc6522354a8786ed0ee97afaa6
blank <hayaken3007@i.softbank.jp>
2023-10-09 00:28:26 +0900

    ルーム用のカバー写真用ストレージ追加

--- apps/api/selfroom-api/app/Constants/StorageSettings.php
+++ apps/api/selfroom-api/app/Constants/StorageSettings.php
@@ -6,4 +6,5 @@ class StorageSettings
 {
   public const USER_PROFILE_STORAGE = 'public/user-profile-photo';
   public const ADMIN_PROFILE_STORAGE = 'public/admin-profile-photo';
+  public const ROOM_COVER_STORAGE = 'public/chat-room-cover-photo';
 }
