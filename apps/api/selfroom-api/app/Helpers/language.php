0ad37b1457f7f8170f274f3f6682b2a6ac7a0abf
blank <hayaken3007@i.softbank.jp>
2023-10-08 11:33:52 +0900

    X-Sr-Languageにja、en以外が来た場合の対応

--- apps/api/selfroom-api/app/Helpers/language.php
+++ apps/api/selfroom-api/app/Helpers/language.php
@@ -2,10 +2,16 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Config;
+
 if (!function_exists('get_lang')) {
 
   function get_lang(): string
   {
-    return request()->headers->get('X-Sr-Language', 'en');
+    $lang = request()->headers->get('X-Sr-Language', 'en');
+    if (array_key_exists($lang, Config::get('languages'))) {
+      return $lang;
+    }
+    return 'en';
   }
 }
