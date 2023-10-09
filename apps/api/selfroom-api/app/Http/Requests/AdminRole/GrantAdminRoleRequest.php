a8733884c81670f806c7ee86acfecb3422ad9698
blank <hayaken3007@i.softbank.jp>
2023-10-09 16:03:42 +0900

    adminロールに関連するフォームリクエスト作成

---
+++ apps/api/selfroom-api/app/Http/Requests/AdminRole/GrantAdminRoleRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\AdminRole;
+
+use App\Enums\Role\AdminRole;
+use App\Http\Requests\ApiRequest;
+use Illuminate\Support\Facades\Gate;
+
+class GrantAdminRoleRequest extends ApiRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return Gate::check(AdminRole::ManageRole->value);
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'roles' => ['required', 'array', 'min:1'],
+      'roles.*' => ['numeric', 'integer', 'exists:App\Models\Roles,role_id'],
+    ];
+  }
+}
---
+++ apps/api/selfroom-api/app/Http/Requests/AdminRole/RevokeAllAdminRoleRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\AdminRole;
+
+use App\Enums\Role\AdminRole;
+use App\Http\Requests\ApiRequest;
+use Illuminate\Support\Facades\Gate;
+
+class RevokeAllAdminRoleRequest extends ApiRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return Gate::check(AdminRole::ManageRole->value);
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+   */
+  public function rules(): array
+  {
+    return [];
+  }
+}
