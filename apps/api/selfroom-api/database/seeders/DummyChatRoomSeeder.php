7ca470c3a31adc821e646c7b8003fceafe0075ee
blank <hayaken3007@i.softbank.jp>
2023-10-13 00:54:04 +0900

    ダミーのチャットルーム生成

---
+++ apps/api/selfroom-api/database/seeders/DummyChatRoomSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class DummyChatRoomSeeder extends Seeder
+{
+  protected $faker;
+
+  public function __construct()
+  {
+    $this->faker = Faker::create('ja_JP');
+  }
+
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $room = \App\Models\ChatRoom::create(
+      [
+        'name' => 'ダミーチャット部屋',
+        'cover_photo_url' => null,
+        'room_key' => app('hash')->make('dummy'),
+      ]
+    );
+    $room->categories()->attach([1, 2, 3]);
+  }
+}
