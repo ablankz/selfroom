0da63d46a95a67e5c393cc4a5f59a9b81eb61ff0
blank <hayaken3007@i.softbank.jp>
2023-10-12 22:12:05 +0900

    ユーザー削除時に関するnum系カラムをデクリメント

--- apps/api/selfroom-api/app/Usecases/User/DeleteUser.php
+++ apps/api/selfroom-api/app/Usecases/User/DeleteUser.php
@@ -5,6 +5,7 @@ namespace App\Usecases\User;
 use App\Enums\ApplicationCode;
 use App\Models\User;
 use App\Usecases\Usecase;
+use Illuminate\Support\Facades\DB;
 
 class DeleteUser extends Usecase
 {
@@ -13,7 +14,28 @@ class DeleteUser extends Usecase
   public function run(
     string $id
   ) {
-    $ret = User::where('user_id', $id)->delete();
+    $ret = DB::transaction(function() use($id) {
+      $user = User::with(['followers', 'followees', 'favoriteRooms', 'currentRoom'])->where('user_id', $id)->first();
+      $user->followers->each(function($follower){
+        $follower->update([
+          'followees' => $follower->followees - 1
+        ]);
+      });
+      $user->followees->each(function($followee){
+        $followee->update([
+          'followers' => $followee->followers - 1
+        ]);
+      });
+      $user->favoriteRooms->each(function($room){
+        $room->update([
+          'favor_num' => $room->favor_num - 1
+        ]);
+      });
+      $user->currentRoom?->update([
+        'user_num' => $user->currentRoom->user_num - 1
+      ]);
+      return $user->delete();
+    });
 
     if (!$ret) {
       return [
