e41e23e6f92e43b9b6beeb9a09ffb7464a30f110
blank <hayaken3007@i.softbank.jp>
2023-10-17 20:25:01 +0900

    paginationDataの作成

--- apps/web/selfroom-web/src/types/response/user/user-followers-response.ts
+++ apps/web/selfroom-web/src/types/response/user/user-followers-response.ts
@@ -1,8 +1,11 @@
 import { User } from '@/types/entity';
 import { ApplicationResponse } from '../application-response';
+import { ApplicationPaginateData } from '../application-paginate-data';
 
 export interface UserFollowerData extends User {
   isFollow: boolean;
 }
 
-export type UserFollowersResponse = ApplicationResponse<UserFollowerData[]>;
+export type UserFollowersResponse = ApplicationResponse<
+  ApplicationPaginateData<UserFollowerData>
+>;
