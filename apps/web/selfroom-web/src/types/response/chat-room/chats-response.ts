e0df533731599c64a9506a676967b68ad6396406
blank <hayaken3007@i.softbank.jp>
2023-10-26 02:29:36 +0900

    cursorページネーション用のtype追加

--- apps/web/selfroom-web/src/types/response/chat-room/chats-response.ts
+++ apps/web/selfroom-web/src/types/response/chat-room/chats-response.ts
@@ -1,6 +1,7 @@
+import { ApplicationCursorPaginateData } from '../application-cursor-paginate-data';
 import { ApplicationResponse } from '../application-response';
 import { Chat } from '@/types/entity';
 
 export interface ChatData extends Chat {}
 
-export type ChatsResponse = ApplicationResponse<ChatData[]>;
+export type ChatsResponse = ApplicationResponse<ApplicationCursorPaginateData<ChatData>>;
