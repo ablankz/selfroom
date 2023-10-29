481f0dc1ffa79afbee88dcd23c5bcdf05a9e472c
blank <hayaken3007@i.softbank.jp>
2023-10-29 15:22:38 +0900

    共通のクエリパラメータ作成

---
+++ apps/web/selfroom-web/src/constants/common-params.ts
@@ -0,0 +1,12 @@
+import { RequestParam } from "@/sections/dashboard/raw-api/view/raw-api-view";
+
+export const GET_COMMON_PARAMS: RequestParam = {
+  limit: 10,
+  offset: 0,
+  order: 'create',
+  order_opt: 'desc',
+} as const;
+
+export const GET_OPT_PARAMS: RequestParam = {
+  total_count: 'with',
+} as const;
